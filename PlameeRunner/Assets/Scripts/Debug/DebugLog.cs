c2c1ba730c1550093d5d97a386d99120c836c63b
lpestl <lpestlname@gmail.com>
2017-10-22 08:20:49 +0300

    Added TouchController and first assets

---
+++ PlameeRunner/Assets/Scripts/Debug/DebugLog.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+/// <summary>
+/// Just temporary work code for verification.
+/// ATTENTION!!!
+/// Do not look here even in the face of death.Blood can go from the eyes !!!
+/// </summary>
+public class DebugLog : MonoBehaviour {
+    
+    public static DebugLog instance;
+
+    void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            DestroyImmediate(gameObject);
+        }
+    }
+    
+    public List<string> messages;
+
+    void Start()
+    {
+        messages = new List<string>();
+        windowRect = new Rect(Screen.width - (float) Screen.width / 4.0f , 10, Screen.width / 4.0f - 10, Screen.height - 20);
+    }
+    
+    void OnEnable()
+    {
+        TouchController.OnTouchDown += TouchController_OnTouchDown;
+        TouchController.OnTouchUp += TouchController_OnTouchUp;
+        TouchController.OnTouchMove += TouchController_OnTouchMove;
+    }
+
+    private void TouchController_OnTouchMove(Vector3 pos, Vector3 delta)
+    {
+        //Print("Pos: " + pos + "; Delta: " + delta);
+    }
+
+    private void TouchController_OnTouchUp(Vector3 pos)
+    {
+        Print("Touch Up: " + pos);
+    }
+
+    private void TouchController_OnTouchDown(Vector3 pos)
+    {
+        Print("Touch Down: " + pos);
+    }
+
+    void OnDisable()
+    {
+        TouchController.OnTouchDown -= TouchController_OnTouchDown;
+        TouchController.OnTouchUp -= TouchController_OnTouchUp;
+        TouchController.OnTouchMove -= TouchController_OnTouchMove;
+    }
+
+    public void Print(string msg)
+    {
+        messages.Add(msg);
+        Debug.Log(msg);
+    }
+
+    private Rect windowRect;
+    void OnGUI()
+    {
+        windowRect = GUI.Window(1, windowRect, GuiConsole, "GUI log");
+    }
+
+    private void GuiConsole(int id)
+    {
+        GUI.Label(new Rect(25, 15, windowRect.width - 50, 25), "Count = " + messages.Count);
+        int y = 15, deltay = 20;
+        for (var i = messages.Count -1; i >0; i--)
+        {
+            GUI.Label(new Rect(10, y += deltay, windowRect.width - 20, windowRect.height - 30), messages[i]);
+        }
+
+        if (messages.Count > 100)
+        {
+            messages.RemoveAt(0);
+        }
+    }
+    
+}
---
+++ PlameeRunner/Assets/Scripts/TouchController.cs
@@ -0,0 +1,103 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+/// <summary>
+/// Put this script on an empty special object on the Scene.
+/// </summary>
+public class TouchController : MonoBehaviour
+{
+#region Delegates-propertys
+    public delegate void EventHandlerTouches(Vector3 pos);
+    public delegate void EventHandlerTouchesMove(Vector3 pos, Vector3 delta);
+
+    public static event EventHandlerTouches OnTouchDown;
+    public static event EventHandlerTouches OnTouchUp;
+    public static event EventHandlerTouchesMove OnTouchMove;
+#endregion
+
+#region Creating Events
+    public static void TouchDown(Vector3 pos)
+    {
+        if (OnTouchDown != null)
+        {
+            OnTouchDown(pos);
+        }
+    }
+
+    public static void TouchUp(Vector3 pos)
+    {
+        if (OnTouchUp != null)
+        {
+            OnTouchUp(pos);
+        }
+    }
+
+    public static void TouchMove(Vector3 pos, Vector3 delta)
+    {
+        if (OnTouchMove != null)
+        {
+            OnTouchMove(pos, delta);
+        }
+    }
+#endregion
+
+#region Functional methods
+    /// <summary>
+    /// Functional methods.
+    /// Getting Inputs info
+    /// </summary>
+
+#if UNITY_EDITOR || UNITY_WINDOWS
+    private bool isTouch = false;
+    private Vector3 lastTouch;
+#endif
+    void Update()
+    {
+#if UNITY_EDITOR || UNITY_WINDOWS
+        
+        // Call event OnTouchDown from mouse
+        if (Input.GetMouseButtonDown(0))
+        {
+            isTouch = true;
+            if (OnTouchDown != null) TouchController.OnTouchDown(Input.mousePosition);
+            lastTouch = Input.mousePosition;
+        }
+
+        // Call event OnTouchMove
+        if (isTouch)
+        {
+            if (OnTouchMove != null) TouchController.OnTouchMove(Input.mousePosition, Input.mousePosition - lastTouch);
+            lastTouch = Input.mousePosition;
+        }
+
+        // Call event OnTouchUp from mouse
+        if (Input.GetMouseButtonUp(0))
+        {
+            isTouch = false;
+            if (OnTouchUp != null) TouchController.OnTouchUp(Input.mousePosition);
+        }
+#else
+                
+        if (Input.touchCount > 0)
+        {
+            switch(Input.GetTouch(0).phase)
+            {
+                case TouchPhase.Began:
+                    if (OnTouchDown != null) TouchController.OnTouchDown(Input.GetTouch(0).position);
+                    break;
+                case TouchPhase.Moved:
+                    if (OnTouchMove != null) TouchController.OnTouchMove(Input.GetTouch(0).position, Input.GetTouch(0).deltaPosition);
+                    break;
+                case TouchPhase.Ended:
+                    if (OnTouchUp != null) TouchController.OnTouchUp(Input.GetTouch(0).position);
+                    break;
+                default:
+                    break;
+            }
+        }
+#endif
+    }
+#endregion
+}
